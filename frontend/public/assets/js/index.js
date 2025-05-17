18a6dc8f690200d4ac29e7d0998d7f0f81eedba6
F1Soda <golik.tima@gmail.com>
2025-05-17 19:34:46 +0500

    UI and user logic improvements

---
+++ frontend/public/assets/js/index.js
@@ -0,0 +1,8 @@
+﻿window.onload = function () {
+    let playerName = localStorage.getItem('playerName');
+
+    if (!playerName) {
+        playerName = generateName();
+        localStorage.setItem('playerName', playerName);
+    }
+};
---
+++ frontend/public/assets/js/settings.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    const input = document.querySelector('#nameInput');
+    let playerName = sessionStorage.getItem('playerName');
+
+    if (!playerName) {
+        playerName = generateName(playerName);
+    }
+    input.value = playerName;
+});

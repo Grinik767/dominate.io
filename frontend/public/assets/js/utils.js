e8799753f8a72c8ac8f753f75dcb624d8d0f6730
F1Soda <golik.tima@gmail.com>
2025-05-19 10:32:54 +0500

    Added game settings page

--- frontend/public/assets/js/utils.js
+++ frontend/public/assets/js/utils.js
@@ -1,3 +1,24 @@
 ﻿function generateName(){
     return "Player" + Math.floor(Math.random()*10000000);
 }
+
+function getHexNeighbors(index, gridSize) {
+    const row = Math.floor(index / gridSize);
+    const col = index % gridSize;
+    const even = row % 2 === 0;
+
+    const offsets = even
+        ? [[-1, 0], [-1, 1], [0, -1], [0, 1], [1, 0], [1, 1]]
+        : [[-1, -1], [-1, 0], [0, -1], [0, 1], [1, -1], [1, 0]];
+
+    const neighbors = offsets.map(([dr, dc]) => {
+        const r = row + dr;
+        const c = col + dc;
+        if (r >= 0 && r < gridSize && c >= 0 && c < gridSize) {
+            return r * gridSize + c;
+        }
+        return null;
+    }).filter(i => i !== null);
+
+    return neighbors;
+}

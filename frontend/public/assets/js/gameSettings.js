7c88e051d988dd8e1e2882f13105efd5d0b01c52
F1Soda <golik.tima@gmail.com>
2025-05-20 12:08:51 +0500

    Game Setting UI improvements

--- frontend/public/assets/js/gameSettings.js
+++ frontend/public/assets/js/gameSettings.js
@@ -1,7 +1,7 @@
 ﻿let botLevels = ["Легко", "Средне", "Тяжело"];
 let botLevelsColors = ["green", "orange", "red"];
 let currentLevelIndex = 1;
-let botsCount = 0;
+let botsCount = 1;
 let playerCount = 1;
 
 const minCountPlayers = 1;
@@ -10,52 +10,99 @@ const maxCountPlayers = 4;
 const minCountBots = 0;
 const maxCountBots = 4;
 
+const countBotsEl = document.querySelector('#botsCount .value-number');
+const countPlayersEl = document.querySelector('#playerCount .value-number');
+const buttonPlayersPlus = document.querySelector('#playerCount .plus');
+const buttonPlayersMinus = document.querySelector('#playerCount .minus');
+const buttonBotsPlus = document.querySelector('#botsCount .plus');
+const buttonBotsMinus = document.querySelector('#botsCount .minus');
+const levelBotEl = document.getElementById("botsLevel");
 
 document.addEventListener("DOMContentLoaded", () => {
-    const levelBotEl = document.getElementById("botsLevel");
-    const botsCountEl = document.getElementById("botsCount");
-    const playerCountEl = document.getElementById("playerCount");
-
     levelBotEl.textContent = botLevels[currentLevelIndex];
     levelBotEl.classList.add(botLevelsColors[currentLevelIndex]);
 
-    botsCountEl.textContent = botsCount;
-    playerCountEl.textContent = playerCount;
+    countBotsEl.textContent = botsCount.toString();
+    countPlayersEl.textContent = playerCount.toString();
 
-    levelBotEl.addEventListener("click", () => {
-        levelBotEl.classList.remove(botLevelsColors[currentLevelIndex]);
-        currentLevelIndex = (currentLevelIndex + 1) % 3;
-        levelBotEl.textContent = botLevels[currentLevelIndex];
-        levelBotEl.classList.add(botLevelsColors[currentLevelIndex]);
-    })
+    levelBotEl.addEventListener("click", changeBotsLevel);
+
+    buttonPlayersPlus.addEventListener("click", () => {
+        if (!buttonPlayersPlus.classList.contains('locked'))
+            changeCountPlayers(1)
+    });
+    buttonPlayersMinus.addEventListener("click", () => {
+        if (!buttonPlayersMinus.classList.contains('locked'))
+            changeCountPlayers(-1)
+    });
+    buttonBotsPlus.addEventListener("click", () => {
+        if (!buttonBotsPlus.classList.contains('locked'))
+            changeCountBots(1)
+    });
+    buttonBotsMinus.addEventListener("click", () => {
+        if (!buttonBotsMinus.classList.contains('locked'))
+            changeCountBots(-1)
+    });
+
+    updateAvailabilityButtons();
 
     const startGameButton = document.getElementById("startGame");
     startGameButton.addEventListener("click", () => {
-
-
         window.location.href = `/game.html?players=${playerCount}&bots=${botsCount}&level=${currentLevelIndex}`;
     })
 });
 
-function changeCount(delta, id) {
-    const countEl = document.getElementById(id);
-    let count = parseInt(countEl.textContent);
+function changeCountPlayers(delta) {
+    let count = playerCount;
 
-    const min = id === "botsCount" ? minCountBots: minCountPlayers;
-    const max = id === "botsCount" ? maxCountBots: maxCountPlayers;
+    count = Math.min(maxCountPlayers, Math.max(minCountPlayers, count + delta));
+    countPlayersEl.textContent = count.toString();
 
-    count = Math.min(max, Math.max(min, count + delta));
-    countEl.textContent = count.toString();
+    playerCount = count;
 
-    if (id === "botsCount"){
-        botsCount = count;
-    } else if (id === "playerCount"){
-        playerCount = count;
+    if (playerCount === minCountPlayers && botsCount === minCountBots) {
+        changeCountBots(1);
+    }
+
+    updateAvailabilityButtons();
+}
+
+function changeCountBots(delta) {
+    let count = botsCount;
+
+    count = Math.min(maxCountBots, Math.max(minCountBots, count + delta));
+    countBotsEl.textContent = count.toString();
+
+    botsCount = count;
+    updateAvailabilityButtons();
+}
+
+function updateAvailabilityButtons() {
+    buttonPlayersMinus.classList.remove('locked');
+    buttonPlayersPlus.classList.remove('locked');
+    if (playerCount === maxCountPlayers) {
+        buttonPlayersPlus.classList.add('locked');
+    } else if (playerCount === minCountPlayers) {
+        buttonPlayersMinus.classList.add('locked');
+    }
+
+    buttonBotsMinus.classList.remove('locked');
+    buttonBotsPlus.classList.remove('locked');
+    if (botsCount === maxCountBots) {
+        buttonBotsPlus.classList.add('locked');
+    } else if (botsCount === minCountBots) {
+        buttonBotsMinus.classList.add('locked');
+    }
+
+    if (playerCount === 1 && botsCount === 1) {
+        buttonBotsMinus.classList.add('locked');
     }
 }
 
-function changeBotsLevel(delta) {
-    const levelEl = document.getElementById("botsLevel");
-    currentLevelIndex = (currentLevelIndex + delta + botLevels.length) % botLevels.length;
-    levelEl.textContent = botLevels[currentLevelIndex];
+function changeBotsLevel() {
+    levelBotEl.classList.remove(botLevelsColors[currentLevelIndex]);
+    currentLevelIndex = (currentLevelIndex + 1) % 3;
+    levelBotEl.textContent = botLevels[currentLevelIndex];
+    levelBotEl.classList.add(botLevelsColors[currentLevelIndex]);
 }
+
